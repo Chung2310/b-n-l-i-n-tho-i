e91395ffaf7b36d7078444a20a8b1d96468e3de5
Chung Đỗ Đức <131289318+Chung2310@users.noreply.github.com>
2026-07-27 16:52:25 +0700

    feat: scope projects and workflows by branch

--- server/interface/project.interface.ts
+++ server/interface/project.interface.ts
@@ -3,6 +3,7 @@ import { Document } from "mongoose";
 export interface IProject extends Document {
   name: string;
   companyCode: string;
+  branchId?: string;
   creatorUid: string;
   createdAt: Date;
 }
