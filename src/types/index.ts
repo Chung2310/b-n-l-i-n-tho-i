c02749200233a39a41550178fd49c5e6e97ce0ec
Chung Đỗ Đức <131289318+Chung2310@users.noreply.github.com>
2026-07-28 14:35:13 +0700

    feat: add recruitment navigation and client API

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from "./integrations";
 export * from "./common";
 export * from "./inventory";
 export * from "./hr";
+export * from "./recruitment";
 export * from "./resource";
 export * from "./dashboard";
